93a237b2462b13fdc0c34147bb41cdfb64418b7c
komal devnani <komaldevnani00@gmail.com>
2020-04-03 14:11:06 +0530

    friendships and cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :friendships, only: [:create, :destroy]
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
   get 'my_portfolio', to:'users#my_portfolio'
+  get 'friends', to:'users#my_friends'
   get 'search_stock', to:'stocks#search'
+  get 'search_friend', to:'users#search'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
