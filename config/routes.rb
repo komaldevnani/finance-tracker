5a27eead6d1f70e45dd06c997c2db9b2c2ca6814
komal devnani <komaldevnani00@gmail.com>
2020-04-10 12:04:19 +0530

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :friendships, only: [:create, :destroy]
   resources :user_stocks, only: [:create, :destroy]
   resources :users, only: [:show]
@@ -6,7 +7,7 @@ Rails.application.routes.draw do
   get 'friends', to:'users#my_friends'
   get 'search_stock', to:'stocks#search'
   get 'search_friend', to:'users#search'
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
