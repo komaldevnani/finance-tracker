7854d866a5f88311b6aa1ecf41e5e48156cb7369
komal devnani <komaldevnani00@gmail.com>
2020-04-29 01:56:31 +0530

    home page styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [ :index ]
   def index
-
+    if current_user
+      redirect_to my_portfolio_path
+    end
   end
 end
